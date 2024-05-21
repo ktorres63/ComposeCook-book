662689f109094dc4ff3644216b0b49af21431b29
ktorres63 <ktorresaro@unsa.edu.pe>
2024-05-20 21:36:22 -0500

    Add style to text

--- app/src/main/java/com/tutorial/composecookbook/RecipeCard.kt
+++ app/src/main/java/com/tutorial/composecookbook/RecipeCard.kt
@@ -2,14 +2,17 @@ package com.tutorial.composecookbook
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
 
@@ -25,9 +28,23 @@ fun RecipeCard(recipe: Recipe) {
                 .height(144.dp)
 
         )
-        Text(recipe.title)
+        Row {
+            Column {
+                Text(
+                    recipe.title,
+                    style = MaterialTheme.typography.headlineLarge,
+                    fontWeight = FontWeight(700)
+                )
+                for (ingredient in recipe.ingredients) {
+                    Text("• $ingredient", style = MaterialTheme.typography.bodyMedium)
+                }
+            }
+            Text(
+                text = recipe.description,
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
     }
-
 }
 
 @Composable

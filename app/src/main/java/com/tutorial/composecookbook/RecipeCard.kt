d6bb46606b0978f285f765a2f14e60ece9e6080b
ktorres63 <ktorresaro@unsa.edu.pe>
2024-05-20 21:55:55 -0500

    Add border radius to RecipeCard

--- app/src/main/java/com/tutorial/composecookbook/RecipeCard.kt
+++ app/src/main/java/com/tutorial/composecookbook/RecipeCard.kt
@@ -1,6 +1,7 @@
 package com.tutorial.composecookbook
 
 import android.widget.Space
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,7 +9,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,39 +24,47 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun RecipeCard(recipe: Recipe) {
-    Column(modifier = Modifier.fillMaxWidth()) {
-        Image(
-            painterResource(recipe.imageResource),
-            contentDescription = recipe.title,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(144.dp)
+    Surface(
+        color = MaterialTheme.colorScheme.surface,
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.secondary),
+        shape = RoundedCornerShape(8.dp),
+        tonalElevation = 2.dp,
+        shadowElevation = 10.dp
+    ) {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Image(
+                painterResource(recipe.imageResource),
+                contentDescription = recipe.title,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(144.dp)
 
-        )
-        Row {
-            Column {
-                Text(
-                    recipe.title,
-                    style = MaterialTheme.typography.headlineLarge,
-                    fontWeight = FontWeight(700)
-                )
-                for (ingredient in recipe.ingredients) {
+            )
+            Row {
+                Column {
                     Text(
-                        text = "• $ingredient",
-                        style = MaterialTheme.typography.bodyMedium
+                        recipe.title,
+                        style = MaterialTheme.typography.headlineLarge,
+                        fontWeight = FontWeight(700)
+                    )
+                    for (ingredient in recipe.ingredients) {
+                        Text(
+                            text = "• $ingredient",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                }
+                Column {
+                    Spacer(modifier = Modifier.height(30.dp))
+                    Text(
+                        text = recipe.description,
+                        style = MaterialTheme.typography.bodySmall,
+                        modifier = Modifier.padding(16.dp)
                     )
                 }
-            }
-            Column {
-                Spacer(modifier = Modifier.height(30.dp))
-                Text(
-                    text = recipe.description,
-                    style = MaterialTheme.typography.bodySmall,
-                    modifier = Modifier.padding(16.dp)
-                )
-            }
 
+            }
         }
     }
 }
